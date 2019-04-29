7e3d594017bbfd799f31ae5b9f561c4345169e8d
xinfu1001 <xinfu1001@xf.com>
2019-04-29 14:20:10 +0800

    xigua

---
+++ createpy.bat
@@ -0,0 +1,10 @@
+virtualenv venv
+venv\\Scripts\\python -m pip install --upgrade pip
+md ".vscode"
+(
+echo {
+echo      "python.pythonPath": "venv\\Scripts\\python"
+echo } 
+) >> ./.vscode/settings.json
+echo venv\\Scripts\\pip3 install -r ./requirements.txt >> pipb.bat
+

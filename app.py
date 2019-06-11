b92137a8557d94bb9e3bd3b1e58985ddbdc306ff
xinfu1001 <xinfu1001@xf.com>
2019-06-11 08:43:17 +0800

    1

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     app.config['log'].info('start app flask ==> host={}, port={}'.format(str(app.config["HOST"]), str(app.config["PORT"])))
     app.run(host=app.config["HOST"], port=app.config["PORT"])
+ 
